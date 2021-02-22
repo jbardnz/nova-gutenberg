ccb142d011489ec44c78deba9ab76d1fc9603411
jbardnz <hello@jarrad.dev>
2021-02-22 21:49:26 +1300

    Update FieldServiceProvider.php

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -33,6 +33,6 @@ class FieldServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../../van-ons/laraberg/config/laraberg.php', 'laraberg');
+        $this->mergeConfigFrom(__DIR__ . '/../../../van-ons/laraberg/srcconfig/laraberg.php', 'laraberg');
     }
 }
